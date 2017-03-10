9a6c6271b9e4681f9357baccc9f18ee5269ed99e
Peter Cummuskey <peterc@aetheric.co.nz>
2017-03-10 16:22:23 +1300

    Adding name attribute to the metadata.

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
+name             "check_mk"
 maintainer       "Fewbytes"
 maintainer_email "chef@fewbytes.com"
 license          "All rights reserved"
 description      "Installs/Configures Check_MK"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.4.2"
+version          "0.4.3"
 
 depends "apache2"
 depends "cluster_service_discovery"
